f7f65c9512e1d69a9f02ee8611349e8338b5928e
Mifaki <agustianto.d19@gmail.com>
2025-02-09 18:43:22 +0700

    feat(init): intialize frontend repository

---
+++ apps/frontend-repo/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import "./globals.css";
+
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import { ThemeProvider } from "@emotion/react";
+import muiTheme from "@/theme/mui";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
+export const metadata: Metadata = {
+  title: "Ebuddy Frontend",
+  description: "Frontend Side of Ebuddy Test",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} antialiased`}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={muiTheme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
---
+++ apps/frontend-repo/src/theme/mui.ts
@@ -0,0 +1,11 @@
+'use client';
+
+import { createTheme } from '@mui/material/styles';
+
+const muiTheme = createTheme({
+  typography: {
+    fontFamily: 'var(--font-roboto)',
+  },
+});
+
+export default muiTheme;
